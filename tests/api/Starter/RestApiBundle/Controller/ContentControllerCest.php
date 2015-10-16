d6a270c539d250d1970eb888e67b02db0c4315d7
Matthew Hansen <inbox@mattyhansen.com>
2015-10-16 11:24:17 +1100

    valid content & default response tests

--- tests/api/Starter/RestApiBundle/Controller/ContentControllerCest.php
+++ tests/api/Starter/RestApiBundle/Controller/ContentControllerCest.php
@@ -1,5 +1,8 @@
 <?php
+
+use Symfony\Component\HttpFoundation\Response;
 use \ApiTester;
+
 
 class ContentControllerCest
 {
@@ -14,10 +17,28 @@ class ContentControllerCest
     // tests
     public function getInvalidContent(ApiTester $i)
     {
-        $i->wantTo('ensure Getting an invalid Content id returns a 404 code');
+        $i->wantTo('ensure getting an invalid Content id returns a 404 code');
 
         $i->sendGET('/api/contents/555');
-        $i->seeResponseCodeIs(404);
+        $i->seeResponseCodeIs(Response::HTTP_NOT_FOUND);
+        $i->seeResponseIsJson();
+    }
+
+    public function getValidContent(ApiTester $i)
+    {
+        $i->wantTo('ensure getting a valid Content id returns a 200 code');
+
+        $i->sendGET('/api/contents/1.json');
+        $i->seeResponseCodeIs(Response::HTTP_OK);
+        $i->seeResponseIsJson();
+    }
+
+    public function ensureDefaultResponseTypeIsJson(ApiTester $i)
+    {
+        $i->wantTo('ensure default response type is json');
+
+        $i->sendGET('/api/contents/1');
+        $i->seeResponseCodeIs(Response::HTTP_OK);
         $i->seeResponseIsJson();
     }
 }
