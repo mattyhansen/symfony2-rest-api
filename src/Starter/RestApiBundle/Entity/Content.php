dfd1ac5ea313fa49dc866296d02a23b0661ccd7e
Matthew Hansen <inbox@mattyhansen.com>
2015-10-18 10:07:37 +1100

    rename table to contents

--- src/Starter/RestApiBundle/Entity/Content.php
+++ src/Starter/RestApiBundle/Entity/Content.php
@@ -8,7 +8,7 @@ use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="Starter\RestApiBundle\Repository\ContentRepository")
- * @ORM\Table(name="content")
+ * @ORM\Table(name="contents")
  */
 class Content implements ContentInterface
 {
